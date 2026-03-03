bf2d327ce675a5f7096756db5e6243220d67524e
Bogdan Tabor <bogdan.tabor@amartus.com>
2026-03-03 20:33:48 +0100

    autosuggestions as prop

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,26 @@ import JavascriptEditor from "./widgets/JavascriptEditor";
 import "./main.css";
 import jsonSchema from "./test/data/input-schema.json";
 
+const autosuggestions = [
+  {
+    label: "@global.request-proxy-1",
+    insertText: "@global.request-proxy-1",
+    documentation: "Request Proxy 1",
+  },
+  {
+    label: "@task.123.output",
+    insertText: "@task.123.output",
+    documentation: "Output of task 123",
+  },
+  {
+    label: "@task.567.output",
+    insertText: "@task.567.output",
+    documentation: "Output of task 567",
+  },
+];
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NodeConfigEditor height="600px" width="800px" schema={jsonSchema} />;
+    <NodeConfigEditor height="600px" width="800px" schema={jsonSchema} autosuggestions={autosuggestions} />;
   </StrictMode>,
 )
--- src/widgets/NodeConfigEditor.jsx
+++ src/widgets/NodeConfigEditor.jsx
@@ -2,25 +2,7 @@ import createNodeConfigEditor from "../editors/nodeConfigEditor";
 import { useEffect, useMemo } from "react";
 import { generateJSONFromSchema } from "./utils";
 
-const autosuggestions = [
-  {
-    label: "@global.request-proxy-1",
-    insertText: "@global.request-proxy-1",
-    documentation: "Request Proxy 1",
-  },
-  {
-    label: "@task.123.output",
-    insertText: "@task.123.output",
-    documentation: "Output of task 123",
-  },
-  {
-    label: "@task.567.output",
-    insertText: "@task.567.output",
-    documentation: "Output of task 567",
-  },
-];
-
-function NodeConfigEditor({ height, width, schema, value }) {
+function NodeConfigEditor({ height, width, schema, value, autosuggestions = [] }) {
   const initialValue = useMemo(() => {
     if (value) return value;
     if (schema) {
@@ -41,7 +23,7 @@ function NodeConfigEditor({ height, width, schema, value }) {
     return () => {
       editor.dispose();
     };
-  }, [schema, initialValue]);
+  }, [schema, initialValue, autosuggestions]);
 
   return <div id="container" style={{ height, width }}></div>;
 }
