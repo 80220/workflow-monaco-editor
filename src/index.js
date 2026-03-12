10eb72b2ce079cfe7c3093c689dd8994e4445704
Bogdan Tabor <bogdan.tabor@amartus.com>
2026-03-12 15:43:56 +0100

    update package.json and vite.config.js for library build configuration and versioning

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as NodeConfigEditor } from "./widgets/NodeConfigEditor";
+export { default as JavascriptEditor } from "./widgets/JavascriptEditor";
+export { default as JobExecutionLogger } from "./widgets/JobExecutionLogger";
