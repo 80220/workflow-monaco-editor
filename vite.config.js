1466e0a7ece8c5706f9584dec9c8a80b9f675c85
Bogdan Tabor <bogdan.tabor@amartus.com>
2026-03-12 15:44:10 +0100

    update package.json and vite.config.js for library configuration and versioning

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,21 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'WorkflowMonacoEditor',
+      fileName: (format) => `workflow-monaco-editor.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'monaco-editor'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'monaco-editor': 'monaco',
+        },
+      },
+    },
+  },
 })
